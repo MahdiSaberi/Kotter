d3cb6e0faaa3df760364201c71ad6ccefc9a0852
MahdiSaberi <mahdisaberi35@gmail.com>
2023-01-24 14:01:25 +0330

    box service completed functions with null handler

--- src/main/kotlin/com/project/kotter/service/BoxEntityService.kt
+++ src/main/kotlin/com/project/kotter/service/BoxEntityService.kt
@@ -2,7 +2,10 @@ package com.project.kotter.service
 
 import com.project.kotter.base.domain.BoxEntity
 import com.project.kotter.base.service.BaseEntityService
+import com.project.kotter.enumeration.BoxType
 
 interface BoxEntityService: BaseEntityService<BoxEntity,Int> {
-    fun findByUuid(uuid: String): BoxEntity
+    fun findByUuid(uuid: String): BoxEntity?
+    fun findByCreator_Id(creator_id: Int): BoxEntity?
+    fun findByType(type: BoxType): BoxEntity?
 }
--- src/main/kotlin/com/project/kotter/service/impl/BoxEntityServiceImpl.kt
+++ src/main/kotlin/com/project/kotter/service/impl/BoxEntityServiceImpl.kt
@@ -2,11 +2,47 @@ package com.project.kotter.service.impl
 
 import com.project.kotter.base.domain.BoxEntity
 import com.project.kotter.base.service.impl.BaseEntityServiceImpl
+import com.project.kotter.enumeration.BoxType
+import com.project.kotter.repository.BoxEntityRepository
 import com.project.kotter.service.BoxEntityService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.lang.Exception
 
+@Service
+@Transactional(readOnly = true)
 class BoxEntityServiceImpl: BaseEntityServiceImpl<BoxEntity, Int>(),BoxEntityService {
 
-    override fun findByUuid(uuid: String): BoxEntity {
-        TODO("Not yet implemented")
+    @Autowired
+    private lateinit var repository: BoxEntityRepository
+    @Transactional
+    override fun findByUuid(uuid: String): BoxEntity? {
+        try {
+            return repository.findByUuid(uuid)
+        }catch (e:Exception){
+            println(e.message)
+            return null
+        }
+    }
+
+    @Transactional
+    override fun findByCreator_Id(creator_id: Int): BoxEntity? {
+        try {
+            return repository.findByCreator_Id(creator_id)
+        }catch (e:Exception){
+            println(e.message)
+            return null
+        }
+    }
+
+    @Transactional
+    override fun findByType(type: BoxType): BoxEntity? {
+        try {
+            return repository.findByType(type)
+        }catch (e:Exception){
+            println(e.message)
+            return null
+        }
     }
 }
