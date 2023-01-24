0b0f1319834c4a24f5aea97094a075c16cc5d15d
MahdiSaberi <mahdisaberi35@gmail.com>
2023-01-24 14:04:34 +0330

    post repository with box entity repository implementation

---
+++ src/main/kotlin/com/project/kotter/repository/PostRepository.kt
@@ -0,0 +1,4 @@
+package com.project.kotter.repository
+
+interface PostRepository: BoxEntityRepository {
+}
--- src/main/kotlin/com/project/kotter/repository/base/BaseUserEntityRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.project.kotter.repository.base
-
-import com.project.kotter.base.domain.UserEntity
-import com.project.kotter.base.repository.BaseEntityRepository
-
-interface BaseUserEntityRepository: BaseEntityRepository<UserEntity,Int> {
-}
