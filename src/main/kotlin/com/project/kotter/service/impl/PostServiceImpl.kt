5ece5257e6650e3d94536fe07381072ea9d3d9e1
MahdiSaberi <mahdisaberi35@gmail.com>
2023-01-24 14:30:40 +0330

    PostServiceImple created and refactored

--- src/main/kotlin/com/project/kotter/service/impl/PostServiceImpl.kt
+++ src/main/kotlin/com/project/kotter/service/impl/PostServiceImpl.kt
@@ -2,7 +2,26 @@ package com.project.kotter.service.impl
 
 import com.project.kotter.domain.Post
 import com.project.kotter.service.PostService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
 
-class PostServiceImpl:BoxEntityServiceImpl {
+@Service
+@Transactional(readOnly = true)
+class PostServiceImpl: BoxEntityServiceImpl<Post, Int>(),PostService {
+    override fun findByTitle(title: String): Post {
+        TODO("Not yet implemented")
+    }
 
+    override fun findByDateOfCreation(date: Date): Post {
+        TODO("Not yet implemented")
+    }
+
+    override fun findByDateOfCreationBetween(date1: Date, date2: Date): Post {
+        TODO("Not yet implemented")
+    }
+
+    override fun findByDateOfCreationYear(year: Int): Post {
+        TODO("Not yet implemented")
+    }
 }
